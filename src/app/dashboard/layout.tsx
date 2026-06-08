061b8f0e5ddb960627d63f07136a8f74b09d56f0
Aniket Tawde <aniikettawde@gmail.com>
2026-06-09 00:06:54 +0530

    type error fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import type { ComponentType } from 'react'
 import { useEffect, useMemo, useState } from 'react'
 import { usePathname, useRouter } from 'next/navigation'
 import Link from 'next/link'
