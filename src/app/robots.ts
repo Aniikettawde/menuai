d89f7ce18875dd3c83886ec9410d737d7d49ea9a
Aniket Tawde <aniikettawde@gmail.com>
2026-07-13 14:38:22 +0530

    qr-generatir updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,11 @@
 import { MetadataRoute } from 'next'
-
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
         userAgent: '*',
         allow: '/',
-        disallow: ['/dashboard/', '/admin/'],
+        disallow: ['/dashboard/', '/admin/', '/products/'],
       },
     ],
     sitemap: 'https://dinezy.in/sitemap.xml',
